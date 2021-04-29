b9bb9b90a98ccc2ab3404f1466b139be94c21e04
Daniel Ibanez <dibanez@iccmu.es>
2021-04-29 15:03:30 +0200

    Bug fixing

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from musif import FeaturesExtractor
 
 if __name__ == "__main__":
 
-    parts = ["flII", "obI", "bn", "hnI", "hnII", "ten"]
-    df_scores = FeaturesExtractor("config.yml").extract("arias/xml", parts_filter=parts)
+    df_scores = FeaturesExtractor("config.yml").extract("arias/xml")
     df_scores.to_csv("myfeatures.csv", index=False)
 
     # df1 = FeaturesExtractor({"split": True, "data_dir": "data"}).from_file("arias/xml/Dem01M-O_piu-1735-Leo[1.01][0430].xml", ["obI", "obII"])
