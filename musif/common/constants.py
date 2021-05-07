c9fc1b861f4a074edd02c70c8eb30845419e6ce0
Daniel Ibanez <dibanez@iccmu.es>
2021-05-07 10:48:49 +0200

    Bugfixing as of 2021-05-07
    Former-commit-id: 89e5863dcd77e15cf2da12dc1f1af8251c248ce5

--- musif/common/constants.py
+++ musif/common/constants.py
@@ -2,3 +2,4 @@ ENCODING = 'utf-8'
 VERSION = "1.0.0"
 CSV_DELIMITER = ","
 VOICE_FAMILY = "voice"
+GENERAL_FAMILY = "general"
--- musif/extract/features/key.py
+++ musif/extract/features/key.py
@@ -9,7 +9,7 @@ from musif.extract.features.prefix import get_corpus_prefix
 
 KEY = "Key"
 KEY_SIGNATURE = "KeySignature"
-KEY_SIGNATURE_GROUPED = "KeySignatureGrouped"
+KEY_SIGNATURE_TYPE = "KeySignatureType"
 MODE = "Mode"
 
 KEY_SIGNATURE_VALUES = ['b', 's']
@@ -21,24 +21,24 @@ def get_score_features(score_data: dict, parts_data: List[dict], cfg: Configurat
     tonality = score_data["tonality"]
     mode = score_data["mode"]
     key_signature = get_key_signature(key)
-    key_signature_grouped = get_key_signature_grouped(key_signature)
+    key_signature_type = get_key_signature_type(key_signature)
 
     return {
         KEY: tonality,
         KEY_SIGNATURE: key_signature,
-        KEY_SIGNATURE_GROUPED: key_signature_grouped,
+        KEY_SIGNATURE_TYPE: key_signature_type,
         MODE: mode,
     }
 
 
 def get_corpus_features(scores_data: List[dict], parts_data: List[dict], cfg: Configuration, scores_features: List[dict], corpus_features: dict) -> dict:
     corpus_prefix = get_corpus_prefix()
-    key_signature_grouped_counter = Counter([score_features[KEY_SIGNATURE_GROUPED] for score_features in scores_features])
+    key_signature_grouped_counter = Counter([score_features[KEY_SIGNATURE_TYPE] for score_features in scores_features])
     mode_counter = Counter([score_features[MODE] for score_features in scores_features])
     features = {}
     for key_signature_group in KEY_SIGNATURE_VALUES:
         count = key_signature_grouped_counter.get(key_signature_group, 0)
-        features[f"{corpus_prefix}{KEY_SIGNATURE_GROUPED}_{key_signature_group}"] = count
+        features[f"{corpus_prefix}{KEY_SIGNATURE_TYPE}_{key_signature_group}"] = count
     for mode in MODE_VALUES:
         count = mode_counter.get(mode, 0)
         features[f"{corpus_prefix}{MODE}_{mode}"] = count
@@ -67,7 +67,7 @@ def get_key_signature(score_key: Key) -> str:
     return key_signature
 
 
-def get_key_signature_grouped(key_signature: str) -> str:
+def get_key_signature_type(key_signature: str) -> str:
     """
     returns the key signature type ('bb) for flats, 'ss' for sharps, and 'nn' for naturals
     """
