22b4fec592e0e03bea7a164f3f71854caafb720b
Daniel Ibanez <dibanez@iccmu.es>
2021-05-20 12:34:49 +0200

    Normalize density per beat

--- musif/extract/features/tempo.py
+++ musif/extract/features/tempo.py
@@ -7,8 +7,7 @@ from music21.expressions import TextExpression
 from music21.stream import Measure
 
 from musif.config import Configuration
-from musif.extract.common import filter_parts_data
-from musif.extract.features.prefix import get_corpus_prefix
+from musif.extract.features.prefix import get_corpus_prefix, get_part_prefix
 
 TEMPO = "Tempo"
 NUMERIC_TEMPO = "NumericTempo"
@@ -16,6 +15,7 @@ TIME_SIGNATURE = "TimeSignature"
 TIME_SIGNATURE_GROUPED = "TimeSignatureGrouped"
 TEMPO_GROUPED_1 = "TempoGrouped1"
 TEMPO_GROUPED_2 = "TempoGrouped2"
+NUMBER_OF_BEATS = "NumberOfBeats"
 
 
 class TempoGroup2(Enum):
@@ -25,15 +25,30 @@ class TempoGroup2(Enum):
     FAST = "Fast"
 
 
+def get_part_features(score_data: dict, part_data: dict, cfg: Configuration, part_features: dict) -> dict:
+
+    part = part_data["part"]
+    time_signature = list(part.getTimeSignatures())[0].ratioString
+    time_signature_grouped = get_time_signature_type(time_signature)
+    number_of_beats = get_number_of_beats(time_signature)
+    features = {
+        TIME_SIGNATURE: time_signature,
+        TIME_SIGNATURE_GROUPED: time_signature_grouped,
+        NUMBER_OF_BEATS: number_of_beats
+    }
+    return features
+
+
 def get_score_features(score_data: dict, parts_data: List[dict], cfg: Configuration, parts_features: List[dict], score_features: dict) -> dict:
 
     # cogemos la part de la voz, y de ahí sacamos el time signature, aparte de devolverla para su posterior uso
     # cambiamos la forma de extraer la voz --- se hace con el atributo de part, 'instrumentSound'. Este atributo
     # indica el tipo de instrumento y por ultimo el nombre. voice.soprano, strings.violin o strings.viola
 
+    score = score_data["score"]
     time_signatures = []
-    for partVoice in score_data["parts"]:
-        m = list(partVoice.getTimeSignatures())
+    for part in score.parts:
+        m = list(part.getTimeSignatures())
         time_signature = m[0].ratioString
         time_signatures.append(time_signature)
 
@@ -50,6 +65,7 @@ def get_score_features(score_data: dict, parts_data: List[dict], cfg: Configurat
     time_signature_grouped = get_time_signature_type(time_signature)
     tg1 = get_tempo_grouped_1(tempo_mark)
     tg2 = get_tempo_grouped_2(tg1).value
+    number_of_beats = get_number_of_beats(time_signature)
 
     return {
         TEMPO: tempo_mark,
@@ -57,6 +73,7 @@ def get_score_features(score_data: dict, parts_data: List[dict], cfg: Configurat
         TIME_SIGNATURE_GROUPED: time_signature_grouped,
         TEMPO_GROUPED_1: tg1,
         TEMPO_GROUPED_2: tg2,
+        NUMBER_OF_BEATS: number_of_beats,
     }
 
 
@@ -173,3 +190,16 @@ def get_tempo_grouped_2(TempoGrouped1) -> TempoGroup2:
     elif TempoGrouped1 in fast:
         return TempoGroup2.FAST
 
+
+def get_number_of_beats(time_signature: str) -> int:
+    time_signature = time_signature.split(",")[0]
+    if time_signature in ['1/2', '1/4', '1/8', '1/16']:
+        return 1
+    if time_signature in ['2/2', '2/4', '2/8', '2/16', '6/8', '6/2', '6/4', '6/16']:
+        return 2
+    if time_signature in ['3/2', '3/4', '3/8', '3/16', '9/2', '9/4', '9/8', '9/16']:
+        return 3
+    if time_signature in ['4/4', '4/2', '4/8', '4/16', 'C', '12/2', '12/4', '12/8', '12/16']:
+        return 4
+    if time_signature in ['8/2', '8/4', '8/8', '8/16']:
+        return 8
