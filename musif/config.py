50fdec34632fe1e635c351531930a26d3763959f
Daniel Ibanez <dibanez@iccmu.es>
2021-06-09 09:19:28 +0200

    Latest improvements
    Former-commit-id: e0a913bf66750fee0827fbf66919407b0a630655

--- musif/config.py
+++ musif/config.py
@@ -17,31 +17,35 @@ _CONFIG_FALLBACK = {
     "intermediate_dir": "intermediate",
     "logs_dir": "logs",
     "log_level": "DEBUG",
-    "sequential": True,
+    "parallel": False,
+    "max_processes": 1,
     "features": [features.value for features in Features],
     "split_keywords": [],
     "level": Level.SCORE.value,
+    "merge_voices": False,
 }
 
 class Configuration:
     def __init__(self, *args, **kwargs):
-        config_data = kwargs or {}
-        if len(kwargs) == 0 and args:
-            if isinstance(args[0], str):
-                config_data = read_object_from_yaml_file(args[0])
-            elif isinstance(args[0], dict):
-                config_data = args[0]
+        config_data = {}
+        if isinstance(args[0], str):
+            config_data = read_object_from_yaml_file(args[0])
+        elif isinstance(args[0], dict):
+            config_data = args[0]
+        config_data.update(kwargs)
         self.data_dir = config_data.get("data_dir", _CONFIG_FALLBACK["data_dir"])
         self.metadata_dir = config_data.get("metadata_dir", _CONFIG_FALLBACK["metadata_dir"])
         self.metadata_id_col = config_data.get("metadata_id_col", _CONFIG_FALLBACK["metadata_id_col"])
         self.logs_dir = config_data.get("logs_dir", _CONFIG_FALLBACK["logs_dir"])
         log_level = config_data.get("log_level", _CONFIG_FALLBACK["log_level"])
         self.intermediate_dir = config_data.get("intermediate_files_dir", _CONFIG_FALLBACK["intermediate_dir"])
-        self.sequential = config_data.get("sequential", _CONFIG_FALLBACK["sequential"])
+        self.parallel = config_data.get("parallel", _CONFIG_FALLBACK["parallel"])
+        self.max_processes = config_data.get("max_processes", _CONFIG_FALLBACK["max_processes"])
         features = config_data.get("features", _CONFIG_FALLBACK["features"])
         self.features = [Features(features_name) for features_name in features]
         self.split_keywords = config_data.get("split_keywords", _CONFIG_FALLBACK["split_keywords"])
         self.level = Level(config_data.get("level", _CONFIG_FALLBACK["level"]))
+        self.merge_voices = config_data.get("merge_voices", _CONFIG_FALLBACK["merge_voices"])
 
         self.scores_metadata = {
             path.basename(file): read_dicts_from_csv(file) for file in glob(path.join(self.metadata_dir, "score", "*.csv"))
