e1e65c139b6d9fa200b015c1c6a5a26cfdfad39d
Daniel Ibanez <daniel@portia.ai>
2021-06-28 12:51:01 +0200

    Simplify extract

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from musif import FeaturesExtractor
+from musif.extract.extract import FilesValidator
 
 if __name__ == "__main__":
 
     didone_filter = ["vnI", "vnII", "va", "bs", "sop", "ten", "alt", "bas"]
     test_filter = ["obI"]
 
-    df_scores = FeaturesExtractor("config.yml", sequential=True).extract(
-        "../Corpus/xml",
-        parts_filter=didone_filter,
-    )
+    FilesValidator("config.yml").validate("../Corpus/xml")
+    df_scores = FeaturesExtractor("config.yml", sequential=True).extract("../Corpus/xml", parts_filter=didone_filter)
     df_scores.to_csv("myfeatures.csv", index=False)
 
     # df1 = FeaturesExtractor({"split": True, "data_dir": "data"}).from_file("arias/xml/Dem01M-O_piu-1735-Leo[1.01][0430].xml", ["obI", "obII"])
