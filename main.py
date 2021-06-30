f0329afc48355aa3438d85eea2791fd7215e567e
Daniel Ibanez <dibanez@iccmu.es>
2021-06-30 03:14:39 +0200

    Latest improvements
    Former-commit-id: 583391a8d1536fd4f0c6e04f9b58cd86178349f5

--- main.py
+++ main.py
@@ -9,9 +9,3 @@ if __name__ == "__main__":
     FilesValidator("config.yml").validate("../Corpus/xml")
     df_scores = FeaturesExtractor("config.yml", sequential=True).extract("../Corpus/xml", parts_filter=didone_filter)
     df_scores.to_csv("myfeatures.csv", index=False)
-
-    # df1 = FeaturesExtractor({"split": True, "data_dir": "data"}).from_file("arias/xml/Dem01M-O_piu-1735-Leo[1.01][0430].xml", ["obI", "obII"])
-    # df2 = FeaturesExtractor(split=True, data_dir="data").from_file("arias/xml/Dem01M-O_piu-1735-Leo[1.01][0430].xml", ["obI", "obII"])
-    # df3 = FeaturesExtractor("/home/daniel/clients/didone/projects/musiF/config.yml").from_file("arias/xml/Dem01M-O_piu-1735-Leo[1.01][0430].xml", ["obI", "obII"])
-    # df4 = FeaturesExtractor("config.yml").from_file("arias/xml/Dem01M-O_piu-1735-Leo[1.01][0430].xml", ["obI", "obII"])
-
