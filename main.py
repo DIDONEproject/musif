06eb3395b07e5bd350ce6d434bc42e05606feab5
Daniel Ibanez <dibanez@iccmu.es>
2021-07-01 05:13:27 +0200

    Create labels script
    Former-commit-id: 864e962d61c666f6686158723ab5981fc87b14a4

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     didone_filter = ["vnI", "vnII", "va", "bs", "sop", "ten", "alt", "bas"]
     test_filter = ["obI"]
 
-    FilesValidator("config.yml").validate("../Corpus/xml")
+    # FilesValidator("config.yml").validate("../Corpus/xml")
     df_scores = FeaturesExtractor("config.yml", sequential=True).extract("../Corpus/xml", parts_filter=didone_filter)
     df_scores.to_csv("myfeatures.csv", index=False)
