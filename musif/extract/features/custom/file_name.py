e9e7386422616d52b9acd7d6f66ebc6d8647b526
Daniel Ibanez <dibanez@iccmu.es>
2021-07-02 10:55:15 +0200

    Fix filename properties to be extracted in reverse order
    Former-commit-id: c2883b8256784f0a72c919d947cb8877f6d240b2

--- musif/extract/features/custom/file_name.py
+++ musif/extract/features/custom/file_name.py
@@ -22,18 +22,34 @@ def get_score_features(score_data: dict, parts_data: List[dict], cfg: Configurat
 
     file_name = basename(score_data["file"])
 
-    opera_title = file_name[0:3]
-    label = file_name.split("-", 2)[0]
-    aria_id = file_name.split("[")[-1].split("]")[0]
-    aria_title = file_name.split("-", 2)[1]
-    year = file_name.split("-", -2)[-2]
-    decade = str(int(year) // 100) + str(int(year[-2:]) // 10) + "0s"
-    act = file_name.split("[", 1)[-1].split(".", 1)[0]
-    scene = file_name.split(".", 1)[-1].split("]", 1)[0]
+    aria_id_end_idx = file_name.rfind("]")
+    aria_id_start_idx = file_name.rfind("[", 0, aria_id_end_idx - 1) + 1
+    aria_id = file_name[aria_id_start_idx: aria_id_end_idx]
+
+    act_scene_end_idx = aria_id_start_idx - 2
+    act_scene_start_idx = file_name.rfind("[", 0, act_scene_end_idx - 1) + 1
+    act_and_scene = file_name[act_scene_start_idx: act_scene_end_idx]
+    act, scene = act_and_scene.split(".")
+
+    composer_end_idx = act_scene_start_idx - 1
+    composer_start_idx = file_name.rfind("-", 0, composer_end_idx - 1) + 1
+    composer = file_name[composer_start_idx: composer_end_idx]
+
+    year_end_idx = composer_start_idx - 1
+    year_start_idx = file_name.rfind("-", 0, year_end_idx - 1) + 1
+    year = int(file_name[year_start_idx: year_end_idx])
+    decade = str(year // 10) + "0s"
+
+    title_end_idx = year_start_idx - 1
+    title_start_idx = file_name.rfind("-", 0, title_end_idx - 1) + 1
+    aria_title = file_name[title_start_idx: title_end_idx]
+
+    aria_labels = file_name[: title_start_idx - 1]
+    opera_prefix = aria_labels[: 3]
 
     return {
-        ARIA_OPERA: opera_title,
-        ARIA_LABEL: label,
+        ARIA_OPERA: opera_prefix,
+        ARIA_LABEL: aria_labels,
         ARIA_ID: aria_id,
         ARIA_NAME: aria_title,
         ARIA_YEAR: year,
