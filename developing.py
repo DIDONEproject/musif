8eb1cd9f320a730952e493434e1ca80866d69a8a
bichomartiano <bichosiete@hotmail.com>
2021-08-24 13:18:10 +0300

    Update

--- developing.py
+++ developing.py
@@ -7,7 +7,9 @@ if __name__ == "__main__":
     # parts = None
 
     # arias_path="./arias/"
-    arias_path="../Corpus/subcorpus"
+    # arias_path="../Corpus/subcorpus"
+    arias_path="../Corpus/fallido"
+
     if os.path.exists("failed_files.txt"):
         os.remove("failed_files.txt")
     df = FeaturesExtractor("myconfig.yml").extract(arias_path, parts)
