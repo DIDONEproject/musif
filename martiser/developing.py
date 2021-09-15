ab13b6d442a53fa087e61de808abb06e8edfb75c
bichomartiano <bichosiete@hotmail.com>
2021-09-15 15:39:39 +0200

    fixing library

--- martiser/developing.py
+++ martiser/developing.py
@@ -1,14 +1,15 @@
 import os
-
 from musif import FeaturesExtractor
-from musif.reports.generate import FeaturesGenerator
+# from musif.extract.extract  import FeaturesExtractor
+
+from musif import FeaturesGenerator
 
 if __name__ == "__main__":
     parts = ["vnI","obI"]
     # parts = None
 
     arias_path="./arias/"
-    arias_path="../Corpus/corpus_filtrado"
+    arias_path="../../Corpus/corpus_filtrado/corpus_filtrado(15-09)"
     # arias_path="../Corpus/corpus_filtrado/fallos"
     # arias_path="../Corpus/corpus_filtrado/fallos/wrong_chords"
     # arias_path="../Corpus/corpus_filtrado/fallos/check"
