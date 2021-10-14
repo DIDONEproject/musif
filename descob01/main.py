eb404f2d2a576dcb4efa71279993323b4d52df01
DaniEscUCM <55737960+DaniEscUCM@users.noreply.github.com>
2021-10-14 17:17:14 +0200

    Add feature Voice Reg.
    Added feature Voice Reg in lyrics

---
+++ descob01/main.py
@@ -0,0 +1,11 @@
+
+from musif import FeaturesExtractor
+
+
+if __name__ == "__main__":
+
+    didone_filter = ["va", "bs", "sop", "ten", "alt", "bar", "bass", "bbar"]
+    df_scores = FeaturesExtractor("config.yml").extract("../arias/Dem02M-In_te-1733-Caldara[1.02][0417].xml",
+                                                        parts_filter=didone_filter)
+
+    print(df_scores["Score_Voice_reg"])
