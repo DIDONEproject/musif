1924eb60372277e7eba293c3b8c3cee4171ca97e
DaniEscUCM <55737960+DaniEscUCM@users.noreply.github.com>
2021-10-25 15:55:02 +0200

    Dynamic and Rhythn Features

--- descob01/main.py
+++ descob01/main.py
@@ -7,7 +7,7 @@ from musif.extract.features.custom.dynamics import DYNMEAN, DYNMEAN_WEIGHTED
 
 if __name__ == "__main__":
     didone_filter = ["vnI","va", "bs", "sop", "ten", "alt", "bar", "bass", "bbar"]
-    df_scores = FeaturesExtractor("config.yml").extract("../arias/Dem02M-In_te-1733-Caldara[1.02][0417].xml",
+    df_scores = FeaturesExtractor("config.yml").extract("../arias/Ale61X-Paventa_del-1758-Piccinni[2.08][0880].xml",
                                                         parts_filter=didone_filter)
     prefix = get_score_prefix()
     #print(df_scores.get(f"{prefix}{RHYTHMINT}"))
