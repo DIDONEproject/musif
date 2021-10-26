ade49cf731d257b48c7951ffbb8fcb2ed6961043
DaniEscUCM <55737960+DaniEscUCM@users.noreply.github.com>
2021-10-26 15:54:51 +0200

    DynGrad and DynAbruptness Features

--- descob01/main.py
+++ descob01/main.py
@@ -1,16 +1,16 @@
 
 from musif import FeaturesExtractor
 from musif.extract.features.prefix import get_score_prefix
-from musif.extract.features.custom.rhythm import RHYTHMINT
-from musif.extract.features.custom.dynamics import DYNMEAN, DYNMEAN_WEIGHTED
+from musif.extract.features.dynamics import DYNMEAN, DYNGRAD, DYNABRUPTNESS
 
 
 if __name__ == "__main__":
     didone_filter = ["vnI","va", "bs", "sop", "ten", "alt", "bar", "bass", "bbar"]
-    df_scores = FeaturesExtractor("config.yml").extract("../arias/Ale61X-Paventa_del-1758-Piccinni[2.08][0880].xml",
+    df_scores = FeaturesExtractor("config.yml").extract("../arias/Dem01M-O_piu-1735-Leo[1.01][0430].xml",
                                                         parts_filter=didone_filter)
     prefix = get_score_prefix()
     #print(df_scores.get(f"{prefix}{RHYTHMINT}"))
     #print(df_scores.get(f"{prefix}{RHYTHMINTSEP}"))
     print(df_scores.get(f"{prefix}{DYNMEAN}"))
-    print(df_scores.get(f"{prefix}{DYNMEAN_WEIGHTED}"))
+    print(df_scores.get(f"{prefix}{DYNGRAD}"))
+    print(df_scores.get(f"{prefix}{DYNABRUPTNESS}"))
