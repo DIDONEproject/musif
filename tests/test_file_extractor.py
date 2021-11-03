c3b7d55b6f1d32b295890a18186fe749493a58ff
DaniEscUCM <55737960+DaniEscUCM@users.noreply.github.com>
2021-11-03 17:11:01 +0100

    Corrections in file extractor tests

--- tests/test_file_extractor.py
+++ tests/test_file_extractor.py
@@ -3,12 +3,18 @@ from os import path
 import pytest
 from musif.extract.extract import extract_files
 
-files_dir_test = path.join("data", "arias_test")
-files_dir_test1 = path.join("data", "arias_tests1")
-files_dir_test_type = path.join("data", "static")
-file_dem1 = "Dem01M-O_piu-1735-Leo[1.01][0430].xml"
-file_dem2 = "Dem02M-In_te-1733-Caldara[1.02][0417].xml"
-file_dem3 = "Did03M-Son_regina-1730-Sarro[1.05][0006].xml"
+data_dir = "data"
+files_dir_test = path.join(data_dir, "arias_test")
+files_dir_test1 = path.join(data_dir, "arias_tests1")
+files_dir_test_type = path.join(data_dir, "static")
+
+file_dem1_test = path.join(files_dir_test, "Dem01M-O_piu-1735-Leo[1.01][0430].xml")
+file_dem2_test = path.join(files_dir_test, "Dem02M-In_te-1733-Caldara[1.02][0417].xml")
+
+file_dem1_test1 = path.join(files_dir_test1, "Dem01M-O_piu-1735-Leo[1.01][0430].xml")
+file_dem2_test1 = path.join(files_dir_test1, "Dem02M-In_te-1733-Caldara[1.02][0417].xml")
+
+file_dem3 = path.join(files_dir_test_type, "Did03M-Son_regina-1730-Sarro[1.05][0006].xml")
 
 
 class TestFilesExtractFiles:
@@ -17,7 +23,7 @@ class TestFilesExtractFiles:
 
     def test_extract_files_one_file(self):
         # Given
-        files_dir = path.join(files_dir_test, file_dem1)
+        files_dir = file_dem1_test
         expected_files = [files_dir]
 
         # When
@@ -29,7 +35,7 @@ class TestFilesExtractFiles:
     def test_extract_files_one_directory(self):
         # Given
         files_dir = files_dir_test
-        expected_files = [path.join(files_dir, file_dem1), path.join(files_dir, file_dem2)]
+        expected_files = [file_dem1_test, file_dem2_test]
 
         # When
         actual_files = extract_files(files_dir)
@@ -39,8 +45,9 @@ class TestFilesExtractFiles:
 
     def test_extract_files_recursive_directory(self):
         # Given
-        files_dir = path.join("data")
+        files_dir = data_dir
         expected_files = []  # It search for .xml files, It's not recursive
+
         # When
         actual_files = extract_files(files_dir)
 
@@ -51,7 +58,7 @@ class TestFilesExtractFiles:
 
     def test_extract_files_several_files(self):
         # Given
-        files_dir = [path.join(files_dir_test, file_dem1), path.join(files_dir_test, file_dem2)]
+        files_dir = [file_dem1_test, file_dem2_test]
         expected_files = files_dir
 
         # When
@@ -63,10 +70,11 @@ class TestFilesExtractFiles:
     def test_extract_files_several_directories(self):
         # Given
         files_dir = [files_dir_test, files_dir_test1]
-        expected_files = [path.join(files_dir_test, file_dem1), path.join(files_dir_test, file_dem2),
-                          path.join(files_dir_test1, file_dem1), path.join(files_dir_test1, file_dem2)]
+        expected_files = [file_dem1_test, file_dem2_test, file_dem1_test1, file_dem2_test1]
+
         # When
         actual_files = extract_files(files_dir)
+
         # then
         assert expected_files == actual_files
 
@@ -74,12 +82,13 @@ class TestFilesExtractFiles:
 
     def test_extract_files_incorrect_type(self):
         # Given
-        with pytest.raises(ValueError):
+        with pytest.raises(TypeError):
             extract_files(0)
 
     def test_extract_non_existing_file(self):
         # Given
         files_dir = "doesntExists"
+
         # Then
         with pytest.raises(ValueError):
             extract_files(files_dir)
@@ -89,7 +98,7 @@ class TestFilesExtractFiles:
     def test_extract_only_good_files_directory(self):
         # Given
         files_dir = files_dir_test_type
-        expected_files = [path.join(files_dir, file_dem3)]
+        expected_files = [file_dem3]
         # When
         actual_files = extract_files(files_dir)
         # Then
@@ -106,10 +115,9 @@ class TestFilesExtractFiles:
 
     def test_extract_only_good_files_list(self):
         # Given
-        files_dir = [path.join(files_dir_test_type, "config.yml"), path.join(files_dir_test_type, file_dem3)]
-        expected_files = [path.join(files_dir_test_type, file_dem3)]
+        files_dir = [path.join(files_dir_test_type, "config.yml"), file_dem3]
+        expected_files = [file_dem3]
         # When
         actual_files = extract_files(files_dir)
         # Then
         assert expected_files == actual_files
-
