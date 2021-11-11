31fdb3f47e16451e61ba75a3f3da980952c9beb6
DaniEscUCM <55737960+DaniEscUCM@users.noreply.github.com>
2021-11-11 17:08:39 +0100

    Change date of person from xml file

--- descob01/main.py
+++ descob01/main.py
@@ -1,19 +1,11 @@
 
-from musif import FeaturesExtractor
-from musif.extract.features.prefix import get_score_prefix
-from musif.extract.features.dynamics import DYNMEAN, DYNGRAD, DYNABRUPTNESS
-from musif.extract.features.rhythm import DOTTEDRHYTHM,DOUBLE_DOTTEDRHYTHM
+from descob01.change_birth_date import change_date_xml
+from musif.extract.extract import parse_musicxml_file
 
 
 if __name__ == "__main__":
-    didone_filter = ["vnI","va", "bs", "sop", "ten", "alt", "bar", "bass", "bbar"]
-    df_scores = FeaturesExtractor("config.yml").extract(["../musif/tests/data/arias_test","../musif/tests/data/arias_test1"],
-                                                        parts_filter=didone_filter)
-    prefix = get_score_prefix()
-    #print(df_scores.get(f"{prefix}{RHYTHMINT}"))
-    #print(df_scores.get(f"{prefix}{RHYTHMINTSEP}"))
-    print(df_scores.get(f"{prefix}{DYNMEAN}"))
-    print(df_scores.get(f"{prefix}{DYNGRAD}"))
-    #print(df_scores.get(f"{prefix}{DYNABRUPTNESS}"))
-    #print(df_scores.get(f"{prefix}{DOTTEDRHYTHM}"))
-    #print(df_scores.get(f"{prefix}{DOUBLE_DOTTEDRHYTHM}"))
+    # change_date_xml("Pietro Metastasio", "1689", "1698", "../arias/xml/Dem01M-O_piu-1735-Leo[1.01][0430].xml")
+    score1 = parse_musicxml_file("../arias/xml/Dem01M-O_piu-1735-Leo[1.01][0430].xml", [])
+    score2 = parse_musicxml_file("resul.xml", [])
+    print(score1.notes.activeElementList[3].content)
+    print(score2.notes.activeElementList[3].content)
