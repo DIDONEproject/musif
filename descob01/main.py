5c043a2c2e9e576e004ec62f606bf817440df1d4
DaniEscUCM <55737960+DaniEscUCM@users.noreply.github.com>
2021-11-15 14:39:49 +0100

    Commit to rebase

--- descob01/main.py
+++ descob01/main.py
@@ -1,11 +1,7 @@
 
-from descob01.change_birth_date import change_date_xml
-from musif.extract.extract import parse_musicxml_file
+from descob01.change_person_year_birth_death import change_year_xml, change_year_musecore
 
 
 if __name__ == "__main__":
-    # change_date_xml("Pietro Metastasio", "1689", "1698", "../arias/xml/Dem01M-O_piu-1735-Leo[1.01][0430].xml")
-    score1 = parse_musicxml_file("../arias/xml/Dem01M-O_piu-1735-Leo[1.01][0430].xml", [])
-    score2 = parse_musicxml_file("resul.xml", [])
-    print(score1.notes.activeElementList[3].content)
-    print(score2.notes.activeElementList[3].content)
+    # change_year_musecore("Pietro Metastasio", "1689", "1698", "change_musecore")
+    change_year_xml("Pietro Metastasio", "1689", "1698", "change_xml")
