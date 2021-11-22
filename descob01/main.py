bcd66f6f04bd3b774f9ea22640e75c47355fdf89
DaniEscUCM <55737960+DaniEscUCM@users.noreply.github.com>
2021-11-22 15:13:19 +0100

    Final script Change xml and musescore person year
    Modify year of birth or death of specific person in xml and musescore files

--- descob01/main.py
+++ descob01/main.py
@@ -1,7 +1,9 @@
 
-from descob01.change_person_year_birth_death import change_year_xml, change_year_musecore
+from descob01.change_person_year_birth_death import change_year_xml, change_year_musecore,change_year_pdf
 
 
 if __name__ == "__main__":
-    # change_year_musecore("Pietro Metastasio", "1689", "1698", "change_musecore")
-    change_year_xml("Pietro Metastasio", "1689", "1698", "change_xml")
+    # change_year_musecore("Pietro Metastasio", "1689", "1698", "musescoreCorpus/musescore")
+     # change_year_xml("Pietro Metastasio", "1689", "1698", "Finale/Finale")
+
+    change_year_pdf("Pietro Metastasio", "1689", "1698", "Ale01M-E_prezzo-1726-Haendel[1.01][0958].pdf")
