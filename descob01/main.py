5bece27efc8a133007a7e097411853813bfbc9e9
DaniEscUCM <55737960+DaniEscUCM@users.noreply.github.com>
2021-11-24 16:23:22 +0100

    Correction in reading and writing files

--- descob01/main.py
+++ descob01/main.py
@@ -1,9 +1,7 @@
 
-from descob01.change_person_year_birth_death import change_year_xml, change_year_musecore,change_year_pdf
+from descob01.change_person_year_birth_death import change_year_xml, change_year_musecore
 
 
 if __name__ == "__main__":
     # change_year_musecore("Pietro Metastasio", "1689", "1698", "musescoreCorpus/musescore")
-     # change_year_xml("Pietro Metastasio", "1689", "1698", "Finale/Finale")
-
-    change_year_pdf("Pietro Metastasio", "1689", "1698", "Ale01M-E_prezzo-1726-Haendel[1.01][0958].pdf")
+      change_year_xml("Pietro Metastasio", "1689", "1698", "../arias/xml/Dem01M-O_piu-1735-Leo[1.01][0430].xml")
