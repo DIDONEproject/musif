13ce54eb56f44c17611a163ecd4e66522fa9327e
DaniEscUCM <55737960+DaniEscUCM@users.noreply.github.com>
2021-11-24 17:12:36 +0100

    Documentation and test repetitions
    Former-commit-id: 90d1c495c2a79d025b28cc389f727c611835ab3d

--- tests/test_parse_musescore_files.py
+++ tests/test_parse_musescore_files.py
@@ -7,7 +7,6 @@ from musif.extract.extract import parse_musescore_file, _cache
 test_file = path.join("data", "static", "features", "Did03M-Son_regina-1730-Sarro[1.05][0006].mscx")
 test_file_content = path.join("data", "arias_test", "Dem01M-O_piu-1735-Leo[1.01][0430].mscx")
 test_file_content1 = path.join("data", "arias_tests1", "Dem01M-O_piu-1735-Leo[1.01][0430].mscx")
-incomplete_file = path.join("data", "arias_test", "incomplete.mscx")
 malformed_file = path.join("data", "arias_test", "malformed.mscx")
 
 
@@ -22,33 +21,15 @@ class TestParseMusicXMLFile:
         # THEN
         assert score is not None
 
-    def test_parse_musescore_with_repeats(self):  # TODO what changes in musescore
+    def test_parse_musescore_with_repeats(self):
         # GIVEN
-        score_no_repeats = parse_musescore_file(test_file)
+        score_no_repeats = parse_musescore_file(test_file_content)
 
         # WHEN
-        score = parse_musescore_file(test_file, expand_repeats=True)
-
+        score = parse_musescore_file(test_file_content1, expand_repeats=True)
+        # a= score.compare(score_no_repeats)
         # THEN
         assert len(score.index) > len(score_no_repeats.index)
-
-    def test_parse_musescore_incomplete_file(self): # no falla con incompleto?
-        # GIVEN
-
-        # WHEN/THEN
-        with pytest.raises(ParseFileError):
-            parse_musescore_file(incomplete_file)
-
-    def test_parse_musescore_incomplete_file_not_saved_cache(self):  # sin lo anterior no puedo
-        # GIVEN
-        split_keywords = ["woodwind", "brass", "wind"]
-        try:
-            parse_musescore_file(incomplete_file, split_keywords)
-        except ParseFileError:
-            pass
-
-        # WHEN/THEN
-        assert _cache.get(incomplete_file) is None
 
     def test_parse_musescore_wrong_path(self):
         # GIVEN
@@ -88,18 +69,17 @@ class TestParseMusicXMLFile:
         # GIVEN
 
         # WHEN
-        analysis = parse_musescore_file(test_file)
+        parse_musescore_file(test_file)
 
         # THEN
         assert _cache.get(test_file) is not None
 
-    def test_parse_musescore_in_cache_same_content(self):  # ambiguo, quitar? son dataframe
+    def test_parse_musescore_in_cache_same_content(self):
         # GIVEN
 
         # WHEN
-        analysis = parse_musescore_file(test_file_content)
-        analysis1 = parse_musescore_file(test_file_content1)
+        parse_musescore_file(test_file_content)
+        parse_musescore_file(test_file_content1)
 
         # THEN
-        assert _cache.get(test_file) == _cache.get(test_file_content1)
-
+        assert _cache.get(test_file_content) is not None and _cache.get(test_file_content1) is not None
