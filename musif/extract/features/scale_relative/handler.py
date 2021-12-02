711f5e5b5b00e65a56066a7456cb9e0e591bcd86
bichomartiano <bichosiete@hotmail.com>
2021-12-02 16:27:07 +0100

    pre-commit to check previous commit
    Former-commit-id: e019d6d179331ae1a142efa199c843cff29f3cdd

---
+++ musif/extract/features/scale_relative/handler.py
@@ -0,0 +1,36 @@
+from typing import List
+
+from musif.config import Configuration
+from musif.extract.common import filter_parts_data
+from musif.extract.constants import DATA_PART_ABBREVIATION
+from musif.extract.features.prefix import get_part_prefix
+from musif.extract.features.scale_relative.utils import get_emphasised_scale_degrees_relative
+from musif.logs import lerr
+from .constants import *
+
+
+def update_part_objects(score_data: dict, part_data: dict, cfg: Configuration, part_features: dict):
+    if 'MS3_score' in score_data:
+        try:
+            notes_per_degree_relative = get_emphasised_scale_degrees_relative(part_data['notes'], score_data)
+            all_degrees = sum(value for value in notes_per_degree_relative.values())
+
+            for key, value in notes_per_degree_relative.items():
+                part_features[DEGREE_COUNT.format(key=key, prefix="")] = value
+                part_features[DEGREE_PER.format(key=key, prefix="")] = value / all_degrees if all_degrees != 0 else 0
+        except Exception as e:
+            lerr(f'Error extracting relative scale degrees: {str(e)}')
+
+
+def update_score_objects(score_data: dict, parts_data: List[dict], cfg: Configuration, parts_features: List[dict], score_features: dict):
+    parts_data = filter_parts_data(parts_data, cfg.parts_filter)
+    
+    if len(parts_data) == 0:
+        return
+
+    for part_data, parts_features in zip(parts_data, parts_features):
+        part_prefix = get_part_prefix(part_data[DATA_PART_ABBREVIATION])
+        for feature in parts_features:
+            if "Degree" in feature:
+                score_features[f"{part_prefix}{feature}"] = parts_features[feature]
+    
