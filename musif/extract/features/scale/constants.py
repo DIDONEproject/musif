f895ca6562098be8369cc6e0805ec40c77d01c94
Daniel Ibanez <dibanez@iccmu.es>
2021-12-07 10:21:39 +0100

    Fix degree features & average duration denominator
    Former-commit-id: d570dfdd8b30629791dfce47eca926f00c85508b

--- musif/extract/features/scale/constants.py
+++ musif/extract/features/scale/constants.py
@@ -1,4 +1,4 @@
 ACCIDENTAL_ABBREVIATION = {"": "", "sharp": "#", "flat": "b", "double-sharp": "x", "double-flat": "bb"}
 
-DEGREE_COUNT = "{prefix}Degree{key}_Count"
-DEGREE_PER = "{prefix}Degree{key}_Per"
+DEGREE_COUNT = "Degree{key}_Count"
+DEGREE_PER = "Degree{key}_Per"
