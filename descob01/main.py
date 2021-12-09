44e44ea597d45fae40f03d5deb0b093153e5f5be
DaniEscUCM <55737960+DaniEscUCM@users.noreply.github.com>
2021-12-09 15:48:50 +0100

    Dynamic score features

--- descob01/main.py
+++ descob01/main.py
@@ -1,7 +1,6 @@
-from descob01.change_person_year_birth_death import change_year_xml, change_year_musecore
-
+from musif.extract.extract import FeaturesExtractor
 
 if __name__ == "__main__":
-    # change_year_musecore("Pietro Metastasio", "1689", "1698", "musescoreCorpus/musescore")
-      change_year_xml("Pietro Metastasio", "1689", "1698", "../arias/xml/Dem01M-O_piu-1735-Leo[1.01][0430].xml")
-
+    df = FeaturesExtractor("config.yml").extract()
+    print(df["Score_DynMean"].values[0])
+    print("hello")
