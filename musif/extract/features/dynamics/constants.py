4adf719947c2b5fc8932ce9ecd8c576a2f07ec39
DaniEscUCM <55737960+DaniEscUCM@users.noreply.github.com>
2021-12-13 16:07:40 +0100

    Fix only correct names in dynamics
    Former-commit-id: c7b2e6b20149ee3b6e7df9ec5cac21f7d461e5c4

--- musif/extract/features/dynamics/constants.py
+++ musif/extract/features/dynamics/constants.py
@@ -11,3 +11,7 @@ REST = "Rest"
 DYNAMIC_VALUES = {"ff": 101, "più f": 96, "f assai": 94, "f": 88, "sempre f": 88, "poco f": 80, "mf": 75,
                   "mp": 62, "p": 49, "sempre p": 49, "p dolce": 49, "p assai": 42, "pp": 36, "sempre pp": 36,
                   "sotto voce assai": 36}
+
+DYNAMIC_LAST_WORDS = ["assai", "dolce"]
+
+DYNAMIC_FIRST_WORD = ["più", "sempre", "poco"]
