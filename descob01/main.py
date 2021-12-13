390aa7fcb643df1e365909b3abca977396023d0b
DaniEscUCM <55737960+DaniEscUCM@users.noreply.github.com>
2021-12-13 14:37:50 +0100

    Score features Rhythm

--- descob01/main.py
+++ descob01/main.py
@@ -2,5 +2,5 @@ from musif.extract.extract import FeaturesExtractor
 
 if __name__ == "__main__":
     df = FeaturesExtractor("config.yml").extract()
-    print(df["Score_DynMean"].values[0])
+    print(df["Score_RhythmInt"].values[0])
     print("hello")
