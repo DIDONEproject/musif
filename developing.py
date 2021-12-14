aca2284da9147c685174628ad69feb9adb82fbff
bichomartiano <bichosiete@hotmail.com>
2021-12-14 17:29:24 +0100

    Delete total key areas

--- developing.py
+++ developing.py
@@ -10,11 +10,11 @@ sys.path.insert(0, "../musif/musif")
 if __name__ == "__main__":
 # 
     data_dir = r'tests/data/static/features'
-    # data_dir = r'arias/'
+    # data_dir = r'arias/error/'
     # 
-    df = FeaturesExtractor("martiser/myconfig.yml", data_dir=data_dir).extract()
-    df.to_csv('martiser/dataframe.csv', index=False)
+    df = FeaturesExtractor("martiser/config.yml", data_dir=data_dir).extract()
 
+    df.to_csv('martiser/dataframe.csv', index=False)
     df=pd.read_csv('martiser/dataframe.csv')
     path = './'
     FeaturesGenerator("martiser/myconfig.yml").generate_reports(df, path, num_factors=1, visualizations=True)
