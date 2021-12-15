8371552a835619f35f298494c3b15fa87d299106
Daniel Ibanez <dibanez@iccmu.es>
2021-12-15 16:48:06 +0100

    Fix chords found in ambitus

--- musif/musicxml/ambitus.py
+++ musif/musicxml/ambitus.py
@@ -1,15 +1,17 @@
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
+from music21.chord import Chord
 from music21.note import Note
 
 
 def get_notes_ambitus(notes: List[Note]) -> Tuple[Note, Note]:
-    lowest_note = notes[0]
-    highest_note = notes[1]
-    for i in range(1, len(notes)):
-        note_index = notes[i].pitch.midi
-        if note_index < lowest_note.pitch.midi:
-            lowest_note = notes[i]
-        if note_index > highest_note.pitch.midi:
-            highest_note = notes[i]
+    first_note = notes[0] if isinstance(notes[0], Chord) else notes[0]
+    lowest_note = first_note
+    highest_note = first_note
+    for note in notes[1:]:
+        current_note = note[0] if isinstance(note, Chord) else note
+        if current_note.pitch.midi < lowest_note.pitch.midi:
+            lowest_note = current_note
+        if current_note.pitch.midi > highest_note.pitch.midi:
+            highest_note = current_note
     return lowest_note, highest_note
