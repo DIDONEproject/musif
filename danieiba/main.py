3d94c379633b72b67a850e639eaeea8f15900829
Daniel Ibanez <dibanez@iccmu.es>
2021-12-15 13:32:39 +0100

    New script for analysis

--- danieiba/main.py
+++ danieiba/main.py
@@ -1,6 +1,8 @@
-from musif import FeaturesExtractor
+from musif import FeaturesExtractor, FilesValidator
 
 if __name__ == "__main__":
 
+    # FilesValidator("config.yml").validate()
     df_scores = FeaturesExtractor("config.yml").extract()
+    df_scores.to_csv("myfeatures.csv", index=False)
     print()
