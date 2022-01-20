160dc854b3c4c98af381b9eaa86de83ba2d70559
bichomartiano <bichosiete@hotmail.com>
2022-01-20 10:52:23 +0100

    adapt voices

--- developing.py
+++ developing.py
@@ -12,8 +12,11 @@ if __name__ == "__main__":
     data_dir = r'tests/data/static/features'
     # data_dir = r'../Corpus_drive400/xml/Ale07M-Se_mai-1752-Perez[1.07][1059].xml'
     musescore_dir=data_dir
+    # V+.
+    data_dir = r'../../_Ana/Music Analysis/xml/corpus_github\xml/Did02M-Diro_che-1749-Jommelli[1.03][0202].xml'
     
-    data_dir = r'../../_Ana/Music Analysis/xml/corpus_github\xml/Ale02M-Vedrai_con-1755-Perez[1.02][0690].xml'
+    #145 Numerals_bvii_Per.1
+    #Key_vi_PercentageMeasures.1 
     #reference
     # data_dir = r'../../_Ana/Music Analysis/xml/corpus_github\xml/Did03M-Son_regina-1724-Sarro[1.05][0001].xml'
 
