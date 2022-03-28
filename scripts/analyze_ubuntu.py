df12ca80b3a198b7dc128dafcdd939ba897d0624
DIDONEproject <data@iccmu.es>
2022-03-28 17:56:19 +0200

    Add poco p value

--- scripts/analyze_ubuntu.py
+++ scripts/analyze_ubuntu.py
@@ -8,8 +8,10 @@ from musif.process.processor import DataProcessor
 if __name__ == "__main__":
     # print('\nUpdating metadata files...')
     # os.system("python scripts/metadata_updater.py")
-    data_dir = r'../Half_Corpus/xml'
-    musescore_dir = r'../../_Ana\Music Analysis/xml/corpus_github/musescore'
+    
+    data_dir = r'../Corpus/xml'
+    musescore_dir = r'../Corpus/musescore'
+    
     # check_file = 'parsed_files_total.csv'
     check_file=None
     name = "features_28_03"
@@ -18,15 +20,4 @@ if __name__ == "__main__":
     dest_path=name+"_extraction.csv"
     df.to_csv(dest_path, index=False)
     
-    p=DataProcessor(dest_path, "scripts/post_process.yml", merge_voices=True)
-    p.process()
-
-    print(p.data.shape)
-    print(p.data.columns[p.data.columns.str.contains('Key')])
-    p._group_keys()
-    print(p.data.columns[p.data.columns.str.contains('Key')])
-    p.delete_unwanted_columns(columns_endswith=['Per'])
-    p.to_csv('final_features_example.csv')
-    #TODO: comprobar que el orden de los metodos es intercambiable y no ffuks everython up
-    print(p.data.shape)
 
