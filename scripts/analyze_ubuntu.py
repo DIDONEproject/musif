091e3c9604942bb775d7e195e47ab1ece0c478e9
DIDONEproject <data@iccmu.es>
2022-03-31 14:04:58 +0200

    update extract and constants and logs

--- scripts/analyze_ubuntu.py
+++ scripts/analyze_ubuntu.py
@@ -9,14 +9,14 @@ if __name__ == "__main__":
     # print('\nUpdating metadata files...')
     # os.system("python scripts/metadata_updater.py")
     
-    data_dir = r'../Corpus/xmlmitad'
+    data_dir = r'../Corpus/xml'
     musescore_dir = r'../Corpus/musescore'
     
     # check_file = 'parsed_files_total.csv'
-    check_file=None
     name = "features_28_03"
+    check_file=name+"_extraction_1.csv"
     df = FeaturesExtractor("scripts/config_drive.yml", data_dir=data_dir, musescore_dir=musescore_dir, check_file=check_file).extract()
-    dest_path=name+"_extraction_1.csv"
+    dest_path=name+"_extraction_dynamics2.csv"
     df.to_csv(dest_path, index=False)
     
 
