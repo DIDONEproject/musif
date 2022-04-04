9a88effbafb036dfad3970166e9f8160fa771b63
DIDONEproject <data@iccmu.es>
2022-04-04 11:26:48 +0200

    upload extraction files
    Former-commit-id: 2bd4fa790eca2fcc5db6047f5fae2b64b8bb2958

--- scripts/analyze_ubuntu.py
+++ scripts/analyze_ubuntu.py
@@ -8,15 +8,15 @@ from musif.process.processor import DataProcessor
 if __name__ == "__main__":
     # print('\nUpdating metadata files...')
     # os.system("python scripts/metadata_updater.py")
-    
+    check_file=None
     data_dir = r'../Corpus/xml'
     musescore_dir = r'../Corpus/musescore'
     
     # check_file = 'parsed_files_total.csv'
-    name = "features_28_03"
+    name = "features_01_04"
     check_file=name+"_extraction_1.csv"
     df = FeaturesExtractor("scripts/config_drive.yml", data_dir=data_dir, musescore_dir=musescore_dir, check_file=check_file).extract()
-    dest_path=name+"_extraction_dynamics2.csv"
+    dest_path=name+"_extraction_2.csv"
     df.to_csv(dest_path, index=False)
     
 
