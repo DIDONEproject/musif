3f3c73db4147bcec0b2d32e6b9abac68b7580421
bichomartiano <bichosiete@hotmail.com>
2022-05-09 11:26:24 +0200

    Finish motion features

--- musif/extract/features/rhythm/constants.py
+++ musif/extract/features/rhythm/constants.py
@@ -3,3 +3,12 @@ RHYTHMINT = "RhythmInt"  # number of notes / number of beats
 # RHYTHMINTSEP = "RhythmIntSep"  # SUM number of notes in a time signature/ beat in a time signature
 DOTTEDRHYTHM = "DottedRhythm"
 DOUBLE_DOTTEDRHYTHM = "DoubleDottedRhythm"
+
+SPEED_AVG_ABS = "Spe_avg_abs"
+ACCELERATION_AVG_ABS = "Acc_avg_abs"
+ASCENDENT_AVERAGE = "Asc_avg"
+DESCENDENT_AVERAGE = "Dsc_avg"
+ASCENDENT_PROPORTION = "Asc_prp"
+DESCENDENT_PROPORTION = "Dsc_prp"
+
+
