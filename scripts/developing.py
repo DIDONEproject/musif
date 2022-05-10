89d49af0ea539d6c2ee3d590cb8393605e2a834b
bichomartiano <bichosiete@hotmail.com>
2022-05-10 11:57:53 +0200

    small fixes

--- scripts/developing.py
+++ scripts/developing.py
@@ -3,6 +3,8 @@ sys.path.insert(0, "../musif/")
 # sys.path.append('../')
 from musif.extract.extract import FeaturesExtractor
 from musif.reports.generate import ReportsGenerator
+from musif.process.processor import DataProcessor
+
 
 import pandas as pd
 
@@ -11,8 +13,7 @@ if __name__ == "__main__":
     data_dir = r'tests/data/static/features'
     musescore_dir=data_dir
 
-    # data_dir = r'../../_Ana/Music Analysis/xml/corpus_github\xml/Ale05M-O_sugli-1740-Caballone[1.05][1139].xml'
-    data_dir = r'../Half_Corpus/'
+    data_dir = r'../../_Ana/Music Analysis/xml/corpus_github\xml/Ale20M-Se_il-1769-Kozeluch[2.15][0761].xml'
     
     #reference
     # data_dir = r'../../_Ana/Music Analysis/xml/corpus_github\xml/Did03M-Son_regina-1724-Sarro[1.05][0001].xml'
@@ -21,8 +22,8 @@ if __name__ == "__main__":
     
     musescore_dir = r'../../_Ana\Music Analysis/xml/corpus_github/musescore'
     
-    df = FeaturesExtractor("scripts/config_drive.yml", data_dir=data_dir, musescore_dir=musescore_dir).extract()
-    df.to_csv('features_extraction_06_05.csv', index=False)
+    df = FeaturesExtractor("scripts/config_tests.yml", data_dir=data_dir, musescore_dir=musescore_dir).extract()
+    df.to_csv('test.csv', index=False)
     # df=pd.read_csv('arias_test.csv')
     path = '.'
-    # ReportsGenerator("scripts/config_tests.yml").generate_reports(df, path, num_factors=0, visualizations=True)
+    ReportsGenerator("scripts/config_tests.yml").generate_reports(df, path, num_factors=0, visualizations=True)
