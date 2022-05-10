cd12626a49311e763f01d63b81c376b45b5c9831
DIDONEproject <data@iccmu.es>
2022-05-10 11:53:25 +0200

    extraction monday

--- scripts/analyze_ubuntu.py
+++ scripts/analyze_ubuntu.py
@@ -10,11 +10,13 @@ if __name__ == "__main__":
     # os.system("python scripts/metadata_updater.py")
     check_file=None
     data_dir = r'../Corpus/xml'
+    # data_dir = r'../Corpus/Half_Corpus'
     musescore_dir = r'../Corpus/musescore'
     
     # check_file = 'parsed_files_total.csv'
-    name = "features_01_04"
-    check_file=name+"_extraction_1.csv"
+    name = "features_09_05"
+    check_file=name+"_extraction.csv"
+    
     df = FeaturesExtractor("scripts/config_drive.yml", data_dir=data_dir, musescore_dir=musescore_dir, check_file=check_file).extract()
     dest_path=name+"_extraction_2.csv"
     df.to_csv(dest_path, index=False)
