d3f7e2c3ad681250e28c38aeec0afbc834357a16
Federico Simonetta <fsimonetta@iccmu.es>
2022-09-06 13:33:37 +0200

    Updated example of test_of_test
    Former-commit-id: e9515016d12cb51fc562d2c6118181c5b67d68b1

--- tests/test_of_test.py
+++ tests/test_of_test.py
@@ -18,19 +18,30 @@ musif.logs.logger().setLevel(musif.logs.LEVEL_INFO)
 rules = pd.read_csv('tests/test_unit_rules.csv', header=0, index_col=0)
 data = pd.read_csv('martiser/extraction_01_08_22_total_alldata.csv', header=0, index_col=False)
 
+def checktype(df, t):
+    """
+    check that all columns in `df` are of type `t`
+
+    returns True or False
+    """
+    if t == 'str':
+        return all(is_string_dtype(x[col]) for col in x.columns)
+    elif t == 'float':
+        return all(is_float_dtype(x[col]) for col in x.columns)
+    elif t == 'int':
+        return all(is_integer_dtype(x[col]) for col in x.columns)
+    elif t == 'bool':
+        return all(is_bool_dtype(x[col]) for col in x.columns)
+
+
 check_functions = {
         'any_na': lambda x, y: x.isna().any()[0],
         'all_na': lambda x, y: x.isna().all()[0],
         'any_0': lambda x, y: (x == 0).any()[0],
         'all_0': lambda x, y: (x == 0).all()[0],
-        'all_str': lambda x, y: all(is_string_dtype(x[col]) for col in x.columns),
-        'all_int': lambda x, y: all(is_integer_dtype(x[col]) for col in x.columns),
-        'all_float': lambda x, y: all(is_float_dtype(x[col]) for col in x.columns),
-        'any_str': lambda x, y: any(is_string_dtype(x[col]) for col in x.columns),
-        'any_int': lambda x, y: any(is_integer_dtype(x[col]) for col in x.columns),
-        'any_float': lambda x, y: any(is_float_dtype(x[col]) for col in x.columns),
         'minval': lambda x, y: (x.select_dtypes([int, float]) < float(y)).any()[0],
-        'maxval': lambda x, y: (x.select_dtypes([int, float]) > float(y)).any()[0]
+        'maxval': lambda x, y: (x.select_dtypes([int, float]) > float(y)).any()[0],
+        'data_type': checktype
         }
 
 def check_rules_column(regex, rules, data, check_functions):
