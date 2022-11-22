9178ec8e35570ea77e638573282ff7db739a0bd4
Federico Simonetta <fsimonetta@iccmu.es>
2022-11-22 17:15:13 +0100

    Maybe fixed error in features.density
    Former-commit-id: 6144672a4e06a7d0237029be7cf4eaa1ca14de59

--- musif/extract/utils.py
+++ musif/extract/utils.py
@@ -606,7 +606,7 @@ def _calculate_total_number_of_beats(time_signatures: list, periods: list) -> in
             # here, time_signature is indexed by j, and not by the content of period...
             # is this correct? shouldn't thay be the opposite?
             # when j == 0, period is also == 0, so it can be skipped
-            period * get_number_of_beats(time_signatures[j])
+            j * get_number_of_beats(time_signatures[period])
             for j, period in enumerate(periods)
         ]
     )
