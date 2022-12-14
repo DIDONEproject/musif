3edcc873a72b4336208f012dbf7be866158ab014
Federico Simonetta <fsimonetta@iccmu.es>
2022-12-14 16:38:22 +0100

    Added __version__ to musif
    Former-commit-id: cf0b656ca93403c3115f8f2ad426c06ba9d01287

--- musif/__init__.py
+++ musif/__init__.py
@@ -1,3 +1,6 @@
 from musif.common._constants import ENCODING
 from musif.extract import FeaturesExtractor
 from musif.process.processor import DataProcessor
+import importlib.metadata
+
+__version__ = importlib.metadata.version(__package__ or __name__)
