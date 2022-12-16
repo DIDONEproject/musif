0cabfdc42e425cc270cd778b7366e8cc2798c273
Federico Simonetta <fsimonetta@iccmu.es>
2022-12-16 15:58:49 +0100

    Fixed xml_dir default value
    Former-commit-id: 94133a4903763fb809b9e60afa73a5f171169c02

--- musif/config.py
+++ musif/config.py
@@ -10,7 +10,7 @@ LOG = "log"
 LOG_FILE_PATH = "log_file"
 FILE_LOG_LEVEL = "file_log_level"
 CONSOLE_LOG_LEVEL = "console_log_level"
-XML_DIR = "data_dir"
+XML_DIR = "xml_dir"
 MUSESCORE_DIR = "musescore_dir"
 CACHE_DIR = "cache_dir"
 PARALLEL = "parallel"
@@ -49,7 +49,7 @@ _CONFIG_LOG_FALLBACK = {
 }
 
 _CONFIG_FALLBACK = {
-    XML_DIR: ".",
+    XML_DIR: None,
     MUSESCORE_DIR: None,
     CACHE_DIR: None,
     PARALLEL: 1,
