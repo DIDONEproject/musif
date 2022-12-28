73be2deb5a3e00e904cab0dfac01f8fc0989d8df
Federico Simonetta <fsimonetta@iccmu.es>
2022-12-28 15:43:34 +0100

    Fixed process.contants docs
    Former-commit-id: 94be40314f43e02e4580be9186f436f2c5f74840

--- musif/process/constants.py
+++ musif/process/constants.py
@@ -1,10 +1,10 @@
 from musif.extract.features.prefix import get_part_prefix
 
-"""Constant to create columns regarding presenc of an instrument or not."""
 PRESENCE = "Presence_of"
+"""Constant to create columns regarding presenc of an instrument or not."""
 
-"""List of prefixes for singers used to find voices columns"""
 voices_list = ["sop", "ten", "alt", "bar", "bbar", "bass"]
+"""List of prefixes for singers used to find voices columns"""
 
-"""List of prefixes 'Part_' + each element of voices_list"""
 voices_list_prefixes = [get_part_prefix(i) for i in voices_list]
+"""List of prefixes `Part_` + each element of voices_list"""
